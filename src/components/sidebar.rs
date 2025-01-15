ce01d831b026a3e69c7b57409ce25c2356d81655
Marcos Fernández <maez@languagewire.com>
2025-01-15 13:32:34 +0100

    Added icons and fixed manifest

--- src/components/sidebar.rs
+++ src/components/sidebar.rs
@@ -14,29 +14,29 @@ pub fn Sidebar() -> Element {
         aside { class: "fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0",
             div { class: "h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800",
                 a { class: "flex items-center ps-2.5 mb-5",
-                    span { class:"self-center text-xl font-semibold whitespace-nowrap dark:text-black",
+                    span { class: "self-center text-xl font-semibold whitespace-nowrap dark:text-black",
                         "DxApp"
                     }
-                },
+                }
                 ul { class: "space-y-2 font-medium",
                     li {
-                        a { class: "flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group",
+                        a {
+                            class: "flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group",
                             onclick: move |_| {
-                                nav.push(Route::Home{});
+                                nav.push(Route::Home {});
                             },
                             Icon {
                                 width: 30,
                                 height: 30,
                                 fill: "black",
                                 icon: HiHome,
-                            },
-                            span { class: "ms-3",
-                                "Home"
                             }
+                            span { class: "ms-3", "Home" }
                         }
-                    },
+                    }
                     li {
-                        a { class: "flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group",
+                        a {
+                            class: "flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group",
                             onclick: move |_| {
                                 nav.push(Route::Blog { id: 1 });
                             },
@@ -45,18 +45,14 @@ pub fn Sidebar() -> Element {
                                 height: 30,
                                 fill: "black",
                                 icon: HiBookOpen,
-                            },
-                            span { class: "ms-3",
-                                "Blog"
                             }
+                            span { class: "ms-3", "Blog" }
                         }
                     }
                 }
             }
-        },
-
-        div { class: "p-4 sm:ml-64",
-            Outlet::<Route> {}
         }
+
+        div { class: "p-4 sm:ml-64", Outlet::<Route> {} }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,26 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-        document::Link { rel: "icon", href: asset!("/assets/favicon.ico") }
+        document::Link { rel: "icon", href: asset!("/assets/icons/favicon.ico") }
+        document::Link {
+            rel: "icon",
+            href: asset!("/assets/icons/favicon-32x32.png"),
+            sizes: "32x32",
+        }
+        document::Link {
+            rel: "icon",
+            href: asset!("/assets/icons/favicon-16x16.png"),
+            sizes: "16x16",
+        }
+        document::Link {
+            rel: "apple-touch-icon",
+            href: asset!("/assets/icons/apple-touch-icon.png"),
+            sizes: "180x180",
+        }
+        document::Link { rel: "manifest", href: asset!("/assets/manifest.json") }
         document::Stylesheet { href: asset!("/assets/styling/main.css") }
-        document::Stylesheet  { href: asset!("/assets/tailwind.css") }
-        document::Stylesheet  { href: "https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" }
+        document::Stylesheet { href: asset!("/assets/tailwind.css") }
+        document::Stylesheet { href: "https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" }
 
         Router::<Route> {}
     }
