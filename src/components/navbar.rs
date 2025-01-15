d59daa2b73ed73df31b0689e4cda36d280b2c2c6
Marcos Fernández <maez@languagewire.com>
2025-01-15 11:09:13 +0100

    Added tailwind + added bottom navbar as an example + added flowbite

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -1,16 +1,49 @@
 use crate::Route;
 use dioxus::prelude::*;
-
-const NAVBAR_CSS: Asset = asset!("/assets/styling/navbar.css");
+use dioxus_free_icons::Icon;
+//Icons
+use dioxus_free_icons::icons::hi_solid_icons::HiHome;
+use dioxus_free_icons::icons::hi_solid_icons::HiBookOpen;
 
 #[component]
 pub fn Navbar() -> Element {
-    rsx! {
-        document::Link { rel: "stylesheet", href: NAVBAR_CSS }
+    let nav = navigator();
+    let NAV_ELEMENTS = 2;
 
-        div { id: "navbar",
-            Link { to: Route::Home {}, "Home" }
-            Link { to: Route::Blog { id: 1 }, "Blog" }
+    let grid_class = format!("grid-cols-{}", NAV_ELEMENTS);
+
+    rsx! {
+        div { id: "navbar", class: "fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600",
+            div { class: "grid h-full max-w-lg {grid_class} mx-auto font-medium",
+                button  { class: "inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group",
+                    onclick: move |_| {
+                        nav.push(Route::Home{});
+                    },
+                    Icon {
+                        width: 30,
+                        height: 30,
+                        fill: "black",
+                        icon: HiHome,
+                    },
+                    span { class: "text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500",
+                        "Home"
+                    }
+                }
+                button  { class: "inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group",
+                    onclick: move |_| {
+                        nav.push(Route::Blog { id: 1 });
+                    },
+                    Icon {
+                        width: 30,
+                        height: 30,
+                        fill: "black",
+                        icon: HiBookOpen,
+                    },
+                    span { class: "text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500",
+                        "Blog"
+                    }
+                }
+            }
         }
 
         Outlet::<Route> {}
--- src/main.rs
+++ src/main.rs
@@ -18,23 +18,17 @@ enum Route {
     Blog { id: i32 },
 }
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
-const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
-
 fn main() {
     dioxus::launch(App);
 }
 
 #[component]
 fn App() -> Element {
-    // Build cool things ✌️
-
     rsx! {
-        // Global app resources
-        document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
-        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+        document::Link { rel: "icon", href: asset!("/assets/favicon.ico") }
+        document::Stylesheet { href: asset!("/assets/styling/main.css") }
+        document::Stylesheet  { href: asset!("/assets/tailwind.css") }
+        document::Stylesheet  { href: "https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" }
 
         Router::<Route> {}
     }
