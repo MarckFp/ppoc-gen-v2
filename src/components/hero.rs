d6de00c1a4ac227324cfcf1b773e47d069d7f752
Marcos Fernández <maez@languagewire.com>
2025-06-03 09:48:53 +0200

    Fixing PWA since it's not needed for the momment

--- src/components/hero.rs
+++ src/components/hero.rs
@@ -6,7 +6,7 @@ const HEADER_SVG: Asset = asset!("/assets/header.svg");
 pub fn Hero() -> Element {
     rsx! {
         div { id: "hero",
-            img { src: HEADER_SVG, id: "header" }
+            img { src: HEADER_SVG, id: "header", alt: "Dioxus Logo" }
             div { id: "links",
                 a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
                 a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
--- src/main.rs
+++ src/main.rs
@@ -23,26 +23,30 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-        document::Link { rel: "icon", href: asset!("/assets/icons/favicon.ico") }
-        document::Link {
-            rel: "icon",
-            href: asset!("/assets/icons/favicon-32x32.png"),
-            sizes: "32x32",
+        head {
+            meta { name: "description", content: "Dioxus template project" }
+            meta { content: "text/html;charset=utf-8", http_equiv: "Content-Type" }
+            meta { name: "viewport", content: "width=device-width, initial-scale=1" }
+            meta { charset: "UTF-8" }
+            document::Link { rel: "icon", href: asset!("/assets/icons/favicon.ico") }
+            document::Link {
+                rel: "icon",
+                href: asset!("/assets/icons/favicon-32x32.png"),
+                sizes: "32x32",
+            }
+            document::Link {
+                rel: "icon",
+                href: asset!("/assets/icons/favicon-16x16.png"),
+                sizes: "16x16",
+            }
+            document::Link {
+                rel: "apple-touch-icon",
+                href: asset!("/assets/icons/apple-touch-icon.png"),
+                sizes: "180x180",
+            }
+            document::Stylesheet { href: asset!("/assets/styling/main.css") }
+            document::Stylesheet { href: asset!("/assets/tailwind.css") }
         }
-        document::Link {
-            rel: "icon",
-            href: asset!("/assets/icons/favicon-16x16.png"),
-            sizes: "16x16",
-        }
-        document::Link {
-            rel: "apple-touch-icon",
-            href: asset!("/assets/icons/apple-touch-icon.png"),
-            sizes: "180x180",
-        }
-        document::Link { rel: "manifest", href: asset!("/assets/manifest.json") }
-        document::Stylesheet { href: asset!("/assets/styling/main.css") }
-        document::Stylesheet { href: asset!("/assets/tailwind.css") }
-
         Router::<Route> {}
     }
 }
